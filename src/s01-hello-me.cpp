27c0df0a7f40cb4fc2458494265f3c76aad8f034
Michał Iwański <s25669@pjwstk.edu.pl>
2021-10-08 10:51:14 +0200

    test commit

---
+++ src/s01-hello-me.cpp
@@ -0,0 +1,15 @@
+/*
+ * The very first program you should write in any new programming language.
+ */
+
+
+#include <iostream>
+
+
+auto main() -> int
+{
+    std::cout << "Hello, World!\n";
+    std::cout << "Michał Iwański";
+    return 0;
+}
+
